953ae4ba83ac1b522b47b5d9ee2b9abca6a78d6f
nicodellolio <nico.dellolio18@gmail.com>
2024-02-26 12:01:07 +0100

    fix jsnack 3

--- jsnack-3/main.js
+++ jsnack-3/main.js
@@ -1,24 +1,24 @@
 let sum = 0
 
-i = 0
-while (i < 10) {
-    let numberString = prompt ('Please insert the number');
-    let number = Number(numberString);
-    i++    
-}
-
-sum = (sum + number);
-alert('La somma dei tuoi numeri è ' + sum)
-
-
-// for (let i = 0; i < 10; i++) {
-//     const numberString = [i];
-//     number = Number(numberString);
-
-
+// i = 0
+// while (i < 10) {
+//     let numberString = prompt ('Please insert a number');
+//     let number = Number(numberString);
+//     i++    
+//     sum = (sum + number);
 // }
 
+// alert('La somma dei tuoi numeri è ' + sum)
 
+
+for (let i = 0; i < 10; i++) {
+    const numberString = prompt('Please insert a number');
+    number = Number(numberString);
+
+    sum = (sum + number)
+}
+
+alert('La somma dei tuoi numeri è ' + sum)
 
 
 
