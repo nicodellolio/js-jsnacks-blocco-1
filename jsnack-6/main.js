e43c57ac24c1577dcd70fcba1c5fcf7138b7880e
nicodellolio <nico.dellolio18@gmail.com>
2024-02-26 12:38:58 +0100

    add for loop

--- jsnack-6/main.js
+++ jsnack-6/main.js
@@ -3,15 +3,22 @@ let N = Number(nString)
 
 let numbersToCube = []
 
-i=0
-while (i < N ) {
-    numbersToCube.push(i)
-    i++
+// i=0
+// while (i < N ) {
+//     numbersToCube.push(i)
+//     i++
 
+//     let cubedNumber = [i] * [i] * [i]
+//     console.log(cubedNumber);
+// }
+
+for (let i = 1; i < N; i++) {
+    numbersToCube.push(i)
+    
     let cubedNumber = [i] * [i] * [i]
     console.log(cubedNumber);
 }
 
-// console.log(numbersToCube);
+
 
 
